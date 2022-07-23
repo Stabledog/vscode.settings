03ea43b35a06769df7015d96ea2211b46d9de680
Les Matheson <lmatheson4@bloomberg.net>
2022-07-23 08:14:22 -0400

    Import settings.json-sort.sh

---
+++ settings.json-sort.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# settings.json-sort.sh
+
+die() {
+    echo "ERROR: $*" >&2
+    exit 1
+}
+
+[[ -f $HOME/bin/settings.json.strip-and-sort.sh ]] || die "Can't find ~/bin/settings.json.strip-and-sort.sh"
+
+source $HOME/bin/settings.json.strip-and-sort.sh
